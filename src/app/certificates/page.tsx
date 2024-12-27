6c01c7245bdca5fd9e2399393d504415e5f09031
abhimykare <abhiraj.mykare@gmail.com>
2024-12-27 22:49:15 +0530

    fix: 404 in cirtificates

---
+++ src/app/certificates/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cirtificates = () => {
+  return (
+    <div>
+      <h1>Certificates</h1>
+      <h2>Comming Soon ...</h2>
+    </div>
+  );
+};
+
+export default Cirtificates;
